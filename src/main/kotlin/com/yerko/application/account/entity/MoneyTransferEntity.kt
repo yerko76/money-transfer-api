cccd108751f880bb269a51413ba0851fe1b0bf74
Yerko Aguirre <yaguirre@thoughtworks.com>
2019-08-28 12:25:36 -0400

    Entity models

--- src/main/kotlin/com/yerko/application/account/entity/MoneyTransferEntity.kt
+++ src/main/kotlin/com/yerko/application/account/entity/MoneyTransferEntity.kt
@@ -1,6 +1,21 @@
 package com.yerko.application.account.entity
 
 import com.yerko.domain.moneytransfer.Money
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import java.math.BigDecimal
 import java.util.*
 
-class MoneyTransferDto(val id:Integer, val fromAccount: UUID, val toAccount: UUID, val transferedAmount: Money)
+
+object MoneyTransactionEntity: Table("money_transaction"){
+    val id: Column<Int> = integer("id").autoIncrement().primaryKey()
+    val fromAccountId: Column<UUID> = uuid("from_account_id")
+    val toAccountId: Column<UUID> = uuid("to_account_id")
+    val amount: Column<BigDecimal> = decimal("amount",15, 6)
+    val currency: Column<String> = varchar("currency", 3)
+}
+
+data class MoneyTransferDto(val id:Int,
+                            val fromAccount: UUID,
+                            val toAccount: UUID,
+                            val transferredAmount: Money)
