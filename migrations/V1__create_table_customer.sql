6425160bb32cf0f925f58ed63ae5cb9d878aaeea
Yerko Aguirre <yaguirre@thoughtworks.com>
2019-08-27 22:11:12 -0400

    Adding Db tables

--- migrations/V1__create_table_customer.sql
+++ migrations/V1__create_table_customer.sql
@@ -1,7 +1,13 @@
 create TABLE customers (
-    customer_id serial primary key,
+    customer_id uuid PRIMARY KEY,
     first_name varchar(100),
     last_name varchar(100),
     email varchar(100) unique,
     active boolean
-)
+);
+
+insert into customers (customer_id,first_name, last_name, email, active) values ('156f6516-33e3-41b6-9335-bbbff54d9094','john', 'doe', 'johndoe@test.com', true);
+insert into customers (customer_id,first_name, last_name, email, active) values ('156f6516-33e3-41b6-9335-bbbff54d9095','lorem', 'ipsum', 'loremipsum@test.com', true);
+insert into customers (customer_id,first_name, last_name, email, active) values ('156f6516-33e3-41b6-9335-bbbff54d9096','some', 'customer', 'somecustomer@test.com', true);
+insert into customers (customer_id,first_name, last_name, email, active) values('156f6516-33e3-41b6-9335-bbbff54d9097','testing', 'user', 'test@test.com', false);
+insert into customers (customer_id,first_name, last_name, email, active) values('156f6516-33e3-41b6-9335-bbbff54d9098','good', 'user', 'gooduser@test.com', true);
---
+++ migrations/V2__create_table_account.sql
@@ -0,0 +1,13 @@
+create TABLE account (
+    account_id uuid PRIMARY KEY,
+    balance numeric(15,6),
+    currency varchar(3),
+    customer_id uuid,
+    created_ad TIMESTAMP DEFAULT NOW(),
+    active boolean,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+insert into account (account_id,customer_id,balance, currency, active) values ('156f6516-33e3-41b6-9335-abcff54d7000','156f6516-33e3-41b6-9335-bbbff54d9094',1000,'USD',true);
+insert into account (account_id,customer_id,balance, currency, active) values ('156f6516-33e3-41b6-9335-abcff54d7001','156f6516-33e3-41b6-9335-bbbff54d9095',2000,'USD',true);
+insert into account (account_id,customer_id,balance, currency, active) values ('156f6516-33e3-41b6-9335-abcff54d7003','156f6516-33e3-41b6-9335-bbbff54d9096',2000,'USD',true);
